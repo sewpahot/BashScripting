5347358e0e1af93c57e9be3456e9db62f350e1f7
Oscar <jamin_91@yahoo.com>
2024-11-17 20:00:44 -0600

    this is the first commit of the file

---
+++ make.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "enter the file name you are lookinf for: " NAME
+
+if [ -f $NAME ]; then  # this checks for the file in the current working directory
+    echo "File exists"
+else
+    echo "this file could not be found in $(pwd)"
+fi
+
+
